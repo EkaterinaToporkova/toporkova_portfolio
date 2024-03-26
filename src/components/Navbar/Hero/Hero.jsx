e62e78adeeba6cd0bbd8e85846070a3d396b984c
Ekaterina Toporkova <90981180+EkaterinaToporkova@users.noreply.github.com>
2024-03-26 22:27:11 +0300

    Update Hero.jsx

--- src/components/Navbar/Hero/Hero.jsx
+++ src/components/Navbar/Hero/Hero.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import { getImageUrl } from '../../../utils';
+import React from "react";
 
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+//тут div - Весь текст левой части
 export const Hero = () => {
   return (
-    <section>
-      {/* div - Весь текст левой части*/}
-      <div>
-        <h1>Привет! Меня зовут Катя Топоркова</h1>
-        <p>
+    <section className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Привет! Меня зовут Катя Топоркова</h1>
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
-        <a href="mailto:myemail@mail.ru">Связаться</a>
+        <a href="mailto:myemail@mail.ru" className={styles.contactBtn}>
+          Связаться
+        </a>
         {/* Нажавший на эту ссылку попадет на свою почту и сможет отправить мне письмо*/}
       </div>
-      <img src={getImageUrl('hero / heroImage.jpg')} alt="hero image of me" />
+      <img src={getImageUrl("hero/heroImage.png")} alt="hero image of me"  className={styles.heroImg} />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
     </section>
   );
 };
+
