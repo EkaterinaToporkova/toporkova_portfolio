4240bbdfc4acce269484398ad2dd015684bf3a46
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2024-03-29 16:47:45 +0300

    Add the Experience component

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Experience.module.css';
+import { getImageUrl } from '../../utils';
+
+//тут div - Весь текст левой части
+export const Experience = () => {
+  return <div>Experience</div>;
+};
