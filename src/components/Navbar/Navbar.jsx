7d741cc3e09ac72b9f045ede21a6474487b53b6c
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2024-04-11 18:02:09 +0300

    Update the Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,37 +7,61 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <a className={styles.title} href="/">
-        Portfolio
-      </a>
       <div className={styles.menu}>
-        <img
-          className={styles.menuBtn}
-          src={
-            menuOpen
-              ? getImageUrl('nav/closeIcon.png')
-              : getImageUrl('nav/menuIcon.png')
-          }
-          alt="menu-button"
-          onClick={() => setMenuOpen(!menuOpen)}
-        />
-        <ul
-          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
-          onClick={() => setMenuOpen(false)}
-        >
-          <li>
-            <a href="#about">Обо мне</a>
-          </li>
-          <li>
-            <a href="#experience">Опыт работы</a>
-          </li>
-          <li>
-            <a href="#projects">Проекты</a>
-          </li>
-          <li>
-            <a href="#contact">Контакты</a>
-          </li>
-        </ul>
+        <div className={styles.line}>
+          <ul className={styles.menuItems}>
+            <li>
+              <a href="#">
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/ios-filled/50/FFFFFF/home.png"
+                  alt="home"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#about">
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/ios-filled/50/FFFFFF/person-female--v1.png"
+                  alt="person-female--v1"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#experience">
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/ios/50/FFFFFF/parse-resumes.png"
+                  alt="parse-resumes"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#projects">
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/hatch/64/FFFFFF/merge-git.png"
+                  alt="merge-git"
+                />
+              </a>
+            </li>
+            <li>
+              <a href="#contact">
+                <img
+                  width="30"
+                  height="30"
+                  src="https://img.icons8.com/glyph-neue/64/FFFFFF/contacts.png"
+                  alt="contacts"
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
