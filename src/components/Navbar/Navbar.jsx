40e7834a63ca03686412d70ea80beee5eb16d781
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2024-04-12 11:06:26 +0300

    Update Navbar.jsx: Update the menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,61 +7,59 @@ export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className={styles.navbar}>
-      <div className={styles.menu}>
-        <div className={styles.line}>
-          <ul className={styles.menuItems}>
-            <li>
-              <a href="#">
-                <img
-                  width="30"
-                  height="30"
-                  src="https://img.icons8.com/ios-filled/50/FFFFFF/home.png"
-                  alt="home"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#about">
-                <img
-                  width="30"
-                  height="30"
-                  src="https://img.icons8.com/ios-filled/50/FFFFFF/person-female--v1.png"
-                  alt="person-female--v1"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#experience">
-                <img
-                  width="30"
-                  height="30"
-                  src="https://img.icons8.com/ios/50/FFFFFF/parse-resumes.png"
-                  alt="parse-resumes"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#projects">
-                <img
-                  width="30"
-                  height="30"
-                  src="https://img.icons8.com/hatch/64/FFFFFF/merge-git.png"
-                  alt="merge-git"
-                />
-              </a>
-            </li>
-            <li>
-              <a href="#contact">
-                <img
-                  width="30"
-                  height="30"
-                  src="https://img.icons8.com/glyph-neue/64/FFFFFF/contacts.png"
-                  alt="contacts"
-                />
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div className={styles.line}>
+        <ul className={styles.menuItems}>
+          <li>
+            <a href="#">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/ios-filled/50/FFFFFF/home.png"
+                alt="home"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/ios-filled/50/FFFFFF/person-female--v1.png"
+                alt="person-female--v1"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#experience">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/ios/50/FFFFFF/parse-resumes.png"
+                alt="parse-resumes"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/hatch/64/FFFFFF/merge-git.png"
+                alt="merge-git"
+              />
+            </a>
+          </li>
+          <li>
+            <a href="#contact">
+              <img
+                width="30"
+                height="30"
+                src="https://img.icons8.com/glyph-neue/64/FFFFFF/contacts.png"
+                alt="contacts"
+              />
+            </a>
+          </li>
+        </ul>
       </div>
     </nav>
   );
