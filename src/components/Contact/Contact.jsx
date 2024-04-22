830b3341caa0d26541134bc1211869f19f540ac6
Ekaterina Toporkova <toporkovaekaterina5@gmail.com>
2024-04-22 10:02:10 +0300

    Update the Contact component: email form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -26,6 +26,30 @@ export const Contact = () => {
   };
   return (
     <footer className={styles.container} id="contact">
+      <div className={styles.conact}>
+        <div className={styles.text}>
+          <h2>Контакты</h2>
+        </div>
+        <ul className={styles.links}>
+          <li className={styles.link}>
+            <img src={getImageUrl('contact/emailIcon.png')} alt="Email icon" />
+            <a href="mailto:myemail@mail.ru">myemail@mail.ru</a>
+          </li>
+          <li className={styles.link}>
+            <img
+              src={getImageUrl('contact/headhunterIcon.png')}
+              alt="headhunter icon"
+            />
+            <a href="#">headhunter.com/EkaterinaToporkova</a>
+          </li>
+          <li className={styles.link}>
+            <img src={getImageUrl('contact/github.png')} alt="GitHub icon" />
+            <a href="https://github.com/EkaterinaToporkova">
+              github.com/EkaterinaToporkova
+            </a>
+          </li>
+        </ul>
+      </div>
       <form ref={form} onSubmit={sendEmail} className={styles.contactForm}>
         <label>Имя</label>
         <input type="text" name="user_name" />
@@ -33,7 +57,7 @@ export const Contact = () => {
         <input type="email" name="user_email" />
         <label>Сообщение</label>
         <textarea name="message" />
-        <input type="submit" value="Send" />
+        <input type="submit" value="Сязаться" />
       </form>
     </footer>
   );
